a51d7dfc87185436e14c38bb8034c30643570254
Cecilia Gomez <cecicecigomez28@gmail.com>
2022-09-22 18:33:54 +0000

    [ADD] Basic Module

---
+++ prueba-biblioteca/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Prueba Biblioteca',
+    
+    'summary': """Prueba para app de Biblioteca""",
+    
+    'description': """
+        Modulo para administrar los prestamos de libros:
+        - Libros 
+        - Cliente
+        - Fecha de pretamo 
+        - Fecha de devolucion prevista 
+        - Fecha de recibido          
+    
+    """,
+    
+    'author': 'Cecilia',
+    
+    'website': '',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+    ],
+}
